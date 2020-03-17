381a623f279e90aac3643f3893b7676ce8377a0d
DrFaust92 <ilia.lazebnik@gmail.com>
2020-03-17 22:30:22 +0200

    remove import

--- aws/resource_aws_opsworks_custom_layer_test.go
+++ aws/resource_aws_opsworks_custom_layer_test.go
@@ -132,11 +132,6 @@ func TestAccAWSOpsworksCustomLayer_noVPC(t *testing.T) {
 				),
 			},
 			{
-				ResourceName:      resourceName,
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
 				Config: testAccAwsOpsworksCustomLayerConfigUpdate(stackName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "name", stackName),
