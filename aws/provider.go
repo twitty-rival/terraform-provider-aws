263b30fdec96fc56d0bf8de72d0a720fa1d14e00
DrFaust92 <ilia.lazebnik@gmail.com>
2020-04-04 23:40:37 +0300

    add new resource to provider

--- aws/provider.go
+++ aws/provider.go
@@ -439,6 +439,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_datasync_location_nfs":                               resourceAwsDataSyncLocationNfs(),
 			"aws_datasync_location_s3":                                resourceAwsDataSyncLocationS3(),
 			"aws_datasync_location_smb":                               resourceAwsDataSyncLocationSmb(),
+			"aws_datasync_location_fsx_windows":                       resourceAwsDataSyncLocationFsxWindows(),
 			"aws_datasync_task":                                       resourceAwsDataSyncTask(),
 			"aws_dax_cluster":                                         resourceAwsDaxCluster(),
 			"aws_dax_parameter_group":                                 resourceAwsDaxParameterGroup(),
